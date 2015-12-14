fd1d31a89d0ab3f2f668281ffa186be775d50a8e
yiftachpb <yiftach.project.budget@gmail.com>
2015-12-14 14:36:20 +0000

    adding data file (initial data structure irrelevent)

---
+++ module/Application/data/budget.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstNameHusband` text,
+  `firstNameWife` text,
+  `lastName` text,
+  `phoneNumber` text,
+  `address` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1
