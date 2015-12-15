9960c022526a20eba8288cf3e3da2a9d1faeed13
yiftachpb <yiftach.project.budget@gmail.com>
2015-12-15 19:59:21 +0000

    added basic data structure

--- module/Application/data/budget.sql
+++ module/Application/data/budget.sql
@@ -1,9 +1,38 @@
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `firstNameHusband` text,
-  `firstNameWife` text,
-  `lastName` text,
-  `phoneNumber` text,
-  `address` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1
+CREATE TABLE expense (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  category text,
+  name text,
+  nickname text,
+  source text,
+  amount text,
+  date date,
+  recurrantInterval text,
+  currentInstallment int,
+  totalInstallments int,
+  status text,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE income (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  category text,
+  name text,
+  nickname text,
+  amount text,
+  payday int,
+  status text,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE categories (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  name text,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE sources (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  name text,
+  nickname text,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
